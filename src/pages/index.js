b5914d82888dcaa8544dc64e5b14c829705a03fb
Aaron Zhang <aaron.zhang@orionhealth.com>
2021-02-25 10:17:44 +1300

    change home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
 import React from "react"
+// import { graphql } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+const IndexPage = () => {
+ return <h1>hello from home page</h1>
 }
+
+// const IndexPage = ({data:{allStrapiHomepage:{nodes}}}) => {
+  
+//   return (
+//     <h1>{nodes[0].title}</h1>
+//   )
+// }
+  
+
+
+// export const query = graphql`
+//   {
+//     allStrapiHomepage {
+//       nodes {
+//         title
+//       }
+//     }
+//   }
+// `
+
+export default IndexPage
