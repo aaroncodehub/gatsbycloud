49703e1aadd393a049e9de9a5d75717c85c840c2
Aaron Zhang <aaron.zhang@orionhealth.com>
2021-02-25 08:38:17 +1300

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        // contentTypes: [`article`, `user`],
+        //If using single types place them in this array.
+        singleTypes: [`homepage`],
+        // Possibility to login with a strapi user, when content types are not publically available (optional).
+      },
+    },
+  ],
+  
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+const IndexPage = ({data:{allStrapiHomepage:{nodes}}}) => {
+  
+  return (
+    <h1>{nodes[0].title}</h1>
+  )
 }
+  
+
+
+export const query = graphql`
+  {
+    allStrapiHomepage {
+      nodes {
+        title
+      }
+    }
+  }
+`
+
+export default IndexPage
