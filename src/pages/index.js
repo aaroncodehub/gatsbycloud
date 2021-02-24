a3fe2eaff6a5dfe7f97fc41dbe286e1ec7d0ef6f
Aaron Zhang <aaron.zhang@orionhealth.com>
2021-02-25 09:44:56 +1300

    change to strapi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,24 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-const IndexPage = () => (
-  <h1>hello</h1>
-)
 
-// const IndexPage = ({data:{allStrapiHomepage:{nodes}}}) => {
+const IndexPage = ({data:{allStrapiHomepage:{nodes}}}) => {
   
-//   return (
-//     <h1>{nodes[0].title}</h1>
-//   )
-// }
+  return (
+    <h1>{nodes[0].title}</h1>
+  )
+}
   
 
 
-// export const query = graphql`
-//   {
-//     allStrapiHomepage {
-//       nodes {
-//         title
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  {
+    allStrapiHomepage {
+      nodes {
+        title
+      }
+    }
+  }
+`
 
 export default IndexPage
